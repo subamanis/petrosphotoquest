4a33a902ae71dbd957ec8ae9411043d264410c3a
Petros_Papa <petrospapa21@gmail.com>
2024-10-09 01:58:45 +0300

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
+import Rock from "./component/Rock.tsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Rock></Rock>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
---
+++ frontend/src/component/Rock.tsx
@@ -0,0 +1,8 @@
+import './Rock.scss';
+import React from 'react';
+
+const Rock: React.FC = () => {
+  return <h1 className="rock-title man">Hello from Rock Component</h1>;
+};
+
+export default Rock;
